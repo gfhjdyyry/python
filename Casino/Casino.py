4155bf0d5d83e3ab7dcf3db7f3aa57e4f9843cb9
imandziy <ivanmandzii@yandex.ru>
2023-08-03 14:26:35 +0300

    Добавили защиту от постороннего ввода

--- Casino/Casino.py
+++ Casino/Casino.py
@@ -7,19 +7,22 @@ print("""Здравствуй, дорогой игрок!
 
 
 while True:
-    z = str(input("Загадайте число, и программа бросит кубик: "))
-    if z == "exit":
-      print("Выход из программы...")
-      break
-    x = randint(1, 6)
-    if x == int(z):
-        print("Правильно")
-        print("Если хотите сыграть снова, введите 'Да':")
-        i = input()
-        if i == "Да":
-            continue
-        else:
-            print("Выход из программы...")
-            break
-    else:
-        print("неправильно, выпало число", x)
+    try:
+         z = str(input("Загадайте число, и программа бросит кубик: "))
+         if z == "exit":
+             print("Выход из программы...")
+             break
+         x = randint(1, 6)
+         if x == int(z):
+            print("Правильно")
+            print("Если хотите сыграть снова, введите 'Да':")
+            i = input()
+            if i == "Да":
+                continue
+            else:
+                print("Выход из программы...")
+                break
+         else:
+              print("неправильно, выпало число", x)
+    except ValueError:
+         print("Введите число")
